da60218efd9072bc5243ca73bc83af4776e8faaf
Sonu <sonusinghmouraya100@gmail.com>
2024-02-09 19:47:26 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Card from './components/Card'
+// import Navbar from './components/Navbar'
+
+
+function App() {
+  return (
+    <>
+    {/* <Navbar/> */}
+    <div className='container'></div>
+    <Card/>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    
+    <nav>
+        <div>
+        <i className="fa-brands fa-youtube"></i>
+        </div>
+
+    </nav>
+  )
+}
+
+export default Navbar
